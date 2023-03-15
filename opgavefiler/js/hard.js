97f5c9fc8ab3c5678657464d3aca0ab8449fe5f7
simo994q <simonkn1@hotmail.com>
2023-03-15 11:30:13 +0100

    fewfwsfe

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -1,8 +1,20 @@
 /* opgave 1 */
 
+let emailInput = document.getElementById('myInput')
+let emailButton = document.getElementById('submitBut')
 
 
+emailButton.addEventListener('click', () => {
+    let userEmail = validateEmail(emailInput.value)
 
+    if (userEmail == true) {
+        emailInput.classList.remove('falseInput')
+        emailInput.classList.add('trueInput')
+    } else {
+        emailInput.classList.remove('trueInput')
+        emailInput.classList.add('falseInput')
+    }
+})
 
 
 
@@ -11,53 +23,92 @@
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regulært udtryk til validering af e-mail
     return emailRegex.test(email); // Returnerer true, hvis e-mail er gyldig, ellers false
-  }
-
-
-  function getData(){
+}
 
 
 
-  
 
-  const myData = [
 
-    {
-        name: 'Elefant',
-        picture: 'assets/img/elephant.jpg',
-        description: 'Loxodonta africana, også kendt som afrikansk elefant, er verdens største landdyr. Den har en grå hud og store ører, som den bruger til at regulere kropstemperaturen og kommunikere med andre elefanter. Afrikanske elefanter lever i store flokke og spiser op til 150 kg planter om dagen. De er også kendt for deres stærke intelligens og følelsesmæssige bånd til deres familie og flok. På grund af ulovlig jagt og tab af levesteder er afrikanske elefanter klassificeret som truede og er beskyttet af lovgivning'
-        , shortDescription: 'Loxodonta africana, også kendt som afrikansk elefant.'
-    },
 
-    {
-        name: 'Tiger',
-        picture: 'assets/img/standard_tiger.jpg',
-        description: 'Panthera tigris, også kendt som tigeren, er en stor kat, der er hjemmehørende i Asien. Tigeren har en gul eller orange pels med mørke striber og har normalt en lang kraftig hale. Der findes forskellige underarter af tigeren, og størrelsen og farven kan variere afhængigt af underarten og habitatet. Tigeren er kendt for sin styrke, hurtighed og smidighed, og den er en top-rovdyr i sit økosystem. Desværre er mange af underarterne af tigeren truede på grund af tab af levesteder og ulovlig jagt, og bevaring af tigeren og dens levesteder er en vigtig bevaringsindsats.',
-        shortDescription: 'Panthera tigris, også kendt som tigeren.'
-    },
 
-    {
-        name: 'Tarantel',
-        picture: 'assets/img/Brachypelma_smithi.jpg',
-        description: 'Brachypelma smithi, også kendt som den mexicansk rødknæs tarantel, er en stor og farverig edderkop, der er hjemmehørende i Mexico. Den har en mørk krop med røde og orange striber på benene og en karakteristisk rød knæled. Brachypelma smithi er en populær art blandt edderkoppeentusiaster på grund af dens smukke farver og rolige natur.',
-        shortDescription: 'Brachypelma smithi, også kendt som den mexicansk rødknæs tarantel.'
-    },
+const galleryContainer = document.getElementById('galleri')
+let data = getData()
 
-    {
-        name: 'Koala',
-        picture: 'assets/img/_WW236934.jpg',
-        description: 'Phascolarctos cinereus, også kendt som koala, er en pungdyrart, der er hjemmehørende i Australien. Den har en blød, tyk pels, store ører og en stor næse, og dens krop er tilpasset til at leve hovedsageligt af eukalyptusblade. Koalaer lever hovedsageligt i trætoppene og er kendt for deres afslappede og søvnige opførsel, da de sover op til 20 timer om dagen.',
-        shortDescription: 'Phascolarctos cinereus, også kendt som koala.'
-    },
+for (obj of data) {
+    console.log(obj);
+    let container = document.createElement('article')
+    let animalImg = document.createElement('img')
+    animalImg.src = obj.picture
+    let animalTitle = document.createElement('h2')
+    animalTitle.innerText = obj.name
+    let animalShortDesc = document.createElement('h3')
+    animalShortDesc.innerText = obj.shortDescription
+    let animalDesc = document.createElement('p')
+    animalDesc.innerText = obj.description
 
-    {
-        name: 'Haj',
-        picture: 'assets/img/great-white.jpg',
-        description: 'Carcharodon carcharias, også kendt som en hvidhaj eller great white haj, er en stor rovdyr, der lever i kystfarvande over hele verden. Den har en grå-blå krop med en trekantet finne på ryggen og en stor kraftig kæbe med skarpe tænder. Carcharodon carcharias er kendt for at være en top-rovdyr og jager primært sæler og fisk. Den er også kendt for dens sjældne, men berygtede, angreb på mennesker, selvom sådanne angreb ofte er utilsigtede og sjældne.',
-        shortDescription: 'Carcharodon carcharias, også kendt som en hvidhaj.'
-    }
-];
-return myData
+    galleryContainer.appendChild(container)
+    container.appendChild(animalImg)
+    container.appendChild(animalTitle)
+    container.appendChild(animalShortDesc)
+    container.appendChild(animalDesc)
+    window.addEventListener('load', () => {
+        container.classList.add('animalContainer')
 
-  }
+    })
+}
 
+
+
+
+
+
+
+
+
+
+console.log(data);
+
+
+
+
+
+function getData() {
+    const myData = [
+
+        {
+            name: 'Elefant',
+            picture: '../../assets/img/elephant.jpg',
+            description: 'Loxodonta africana, også kendt som afrikansk elefant, er verdens største landdyr. Den har en grå hud og store ører, som den bruger til at regulere kropstemperaturen og kommunikere med andre elefanter. Afrikanske elefanter lever i store flokke og spiser op til 150 kg planter om dagen. De er også kendt for deres stærke intelligens og følelsesmæssige bånd til deres familie og flok. På grund af ulovlig jagt og tab af levesteder er afrikanske elefanter klassificeret som truede og er beskyttet af lovgivning'
+            , shortDescription: 'Loxodonta africana, også kendt som afrikansk elefant.'
+        },
+
+        {
+            name: 'Tiger',
+            picture: '../../assets/img/standard_tiger.jpg',
+            description: 'Panthera tigris, også kendt som tigeren, er en stor kat, der er hjemmehørende i Asien. Tigeren har en gul eller orange pels med mørke striber og har normalt en lang kraftig hale. Der findes forskellige underarter af tigeren, og størrelsen og farven kan variere afhængigt af underarten og habitatet. Tigeren er kendt for sin styrke, hurtighed og smidighed, og den er en top-rovdyr i sit økosystem. Desværre er mange af underarterne af tigeren truede på grund af tab af levesteder og ulovlig jagt, og bevaring af tigeren og dens levesteder er en vigtig bevaringsindsats.',
+            shortDescription: 'Panthera tigris, også kendt som tigeren.'
+        },
+
+        {
+            name: 'Tarantel',
+            picture: '../../assets/img/Brachypelma_smithi.jpg',
+            description: 'Brachypelma smithi, også kendt som den mexicansk rødknæs tarantel, er en stor og farverig edderkop, der er hjemmehørende i Mexico. Den har en mørk krop med røde og orange striber på benene og en karakteristisk rød knæled. Brachypelma smithi er en populær art blandt edderkoppeentusiaster på grund af dens smukke farver og rolige natur.',
+            shortDescription: 'Brachypelma smithi, også kendt som den mexicansk rødknæs tarantel.'
+        },
+
+        {
+            name: 'Koala',
+            picture: '../../assets/img/_WW236934.jpg',
+            description: 'Phascolarctos cinereus, også kendt som koala, er en pungdyrart, der er hjemmehørende i Australien. Den har en blød, tyk pels, store ører og en stor næse, og dens krop er tilpasset til at leve hovedsageligt af eukalyptusblade. Koalaer lever hovedsageligt i trætoppene og er kendt for deres afslappede og søvnige opførsel, da de sover op til 20 timer om dagen.',
+            shortDescription: 'Phascolarctos cinereus, også kendt som koala.'
+        },
+
+        {
+            name: 'Haj',
+            picture: '../../assets/img/great-white.jpg',
+            description: 'Carcharodon carcharias, også kendt som en hvidhaj eller great white haj, er en stor rovdyr, der lever i kystfarvande over hele verden. Den har en grå-blå krop med en trekantet finne på ryggen og en stor kraftig kæbe med skarpe tænder. Carcharodon carcharias er kendt for at være en top-rovdyr og jager primært sæler og fisk. Den er også kendt for dens sjældne, men berygtede, angreb på mennesker, selvom sådanne angreb ofte er utilsigtede og sjældne.',
+            shortDescription: 'Carcharodon carcharias, også kendt som en hvidhaj.'
+        }
+    ];
+    return myData
+}
